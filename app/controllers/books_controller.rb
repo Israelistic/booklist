e1c25622ca1cb78cfb7cb1893239a54e236c49a0
Israelistic <haguyle@gmail.com>
2018-07-09 21:13:18 -0400

    added an index view and index method in books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,11 @@
 class BooksController < ApplicationController
+    def index
+        @books = Book.all
+        respond_to do |format|
+            format.html
+            format.text
+        end
+    end
 
 
 end
